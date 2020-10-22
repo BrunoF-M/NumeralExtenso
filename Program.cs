db22f922ff66d8b6e0ee2149226a07db21700e0e
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-10-22 04:07:11 -0300

    Resolução do exercício

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Figgle;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.ForegroundColor=ConsoleColor.DarkCyan;
+            Console.WriteLine("+-=-=Numeral por Extenso=-=-+");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            int numero;
+
+            Console.Write("Digite um número..: ");
+            numero = int.Parse(Console.ReadLine());
+
+            string numeral1 = numero.ToWords();
+
+            Console.WriteLine();
+            
+            Console.WriteLine($"{numeral1}");
+
+            Console.WriteLine();
+            Console.Write("enter to finish...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
